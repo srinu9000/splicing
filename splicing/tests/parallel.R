1983fa8e0f42a424bddf30028c040566fbbe4556
Gabor Csardi <csardi.gabor@gmail.com>
2014-07-29 15:19:25 -0400

    Skip test case if snow package is not available

--- splicing/tests/parallel.R
+++ splicing/tests/parallel.R
@@ -1,34 +1,37 @@
 
-library(splicing)
-library(snow)
+if (check_for_package("snow", message = ", test case skipped",
+                      fail = FALSE)) {
 
-genelist <- lapply(seq(10,100,by=10), function(x) {
-  createGene(exons=list(c(1,1000), c(2001, 2000+x), c(4001,5000)),
-             isoforms=list(c(1,2,3), c(1,3)), id=sprintf("gene-%d", x))
-})
+  library(splicing)
 
-genes <- do.call(merge, genelist)
+  genelist <- lapply(seq(10,100,by=10), function(x) {
+    createGene(exons=list(c(1,1000), c(2001, 2000+x), c(4001,5000)),
+               isoforms=list(c(1,2,3), c(1,3)), id=sprintf("gene-%d", x))
+  })
 
-set.seed(42)
-readlist <- lapply(seq_along(genelist), function(x)
-                   simulateReads(geneStructure=genes, gene=x, 
-                                 qname=sprintf("read-%i-%%i", x),
-                                 expression=c(2/10, 8/10),
-                                 noReads=1000L, readLength=35))
+  genes <- do.call(merge, genelist)
 
-reads <- do.call(mergeReads, readlist)
+  set.seed(42)
+  readlist <- lapply(seq_along(genelist), function(x)
+                    simulateReads(geneStructure=genes, gene=x,
+                                  qname=sprintf("read-%i-%%i", x),
+                                  expression=c(2/10, 8/10),
+                                  noReads=1000L, readLength=35))
 
-samfile <- paste(tempfile(), sep="", ".sam")
-bamfile <- paste(tempfile(), sep="", ".bam")
-bamfile2 <- tempfile()
-writeSAM(reads, samfile)
-SAMFile2BAMFile(samfile, bamfile)
-sortBAMFile(bamfile, bamfile2)
-indexBAMFile(paste(bamfile2, sep="", ".bam"))
+  reads <- do.call(mergeReads, readlist)
 
-cl <- makeCluster(10)
+  samfile <- paste(tempfile(), sep="", ".sam")
+  bamfile <- paste(tempfile(), sep="", ".bam")
+  bamfile2 <- tempfile()
+  writeSAM(reads, samfile)
+  SAMFile2BAMFile(samfile, bamfile)
+  sortBAMFile(bamfile, bamfile2)
+  indexBAMFile(paste(bamfile2, sep="", ".bam"))
 
-runMISO(genes, paste(bamfile2, sep="", ".bam"), snowCluster=cl, seed=42,
-        verbose=FALSE)
+  cl <- makeCluster(10)
 
-stopCluster(cl)
+  runMISO(genes, paste(bamfile2, sep="", ".bam"), snowCluster=cl, seed=42,
+          verbose=FALSE)
+
+  stopCluster(cl)
+}
