f13a1ef85cc884db6a1451bd8614003727f09d9b
Gabor Csardi <csardi.gabor@gmail.com>
2014-07-29 15:00:30 -0400

    Fix .onLoad, etc.
    It seems that the package unloads cleanly now.

--- splicing/R/package.R
+++ splicing/R/package.R
@@ -1,18 +1,6 @@
 
-.onAttach <- function(library, pkg) {
-  invisible()
-}
-
-.onLoad <- function(dir, package) {
-  library.dynam("splicing", package, dir, local=FALSE)
-}
-
-.onUnload <- function(libpath) {
-  library.dynam.unload("splicing", libpath)
-}
-
-.Last.lib <- function(libpath) {
-  .onUnload(libpath)
+.onLoad <- function(lib, pkg) {
+  library.dynam("splicing", pkg, lib)
 }
 
 splicing_version <- function() {
